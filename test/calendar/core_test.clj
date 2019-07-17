c5b6f13377f65495f6146d8601173d2a64c31762
sto <son.c.to@gmail.com>
2019-07-17 13:45:05 -0400

    report-overlapping

--- test/calendar/core_test.clj
+++ test/calendar/core_test.clj
@@ -5,10 +5,14 @@
   (- (:end evt)
      (:start evt)))
 
-(defn valid-event? [evt]
+(defn valid-event?
+  "a valid event must have a positive duration"
+  [evt]
   (-> evt duration pos?))
 
-(defn sort-events [events]
+(defn sort-events
+  "sort events in ascending order"
+  [events]
   (sort (fn [evt1 evt2]
           (- (:start evt1)
              (:start evt2)))
@@ -23,6 +27,18 @@
         start-b (:start evt-b)
         end-b (:end evt-b)]
     (<= start-b end-a)))
+
+(defn report-overlapping [events]
+  (let [sorted-events (sort-events events)]
+    (for [e1 sorted-events
+          :let [other (remove #(= % e1) sorted-events)
+                _ (prn "other = " other)
+                pairs (remove nil? (for [e2 (remove #(= % e1) sorted-events)]
+                                     (if (overlap? e1 e2)
+                                       #{e1 e2}
+                                       nil)))
+                _ (prn "pairs=" pairs)]]
+      pairs)))
 
 (deftest overlap-test
   (testing "no overlap"
@@ -49,9 +65,23 @@
 
     (is (overlap?
          {:start 1 :end 5}
-         {:start 2 :end 4})))
-  )
+         {:start 2 :end 4}))))
 
+(deftest report-test
+  (testing "no overlapping events"
+    (let [events [{:start 1 :end 3}
+                  {:start 4 :end 6}
+                  {:start 7 :end 10}]]
+      (is (empty? (report-overlapping events)))))
+
+  (testing "two overlapping event"
+    (let [events [{:start 1 :end 10}
+                  {:start 1 :end 3}
+                  {:start 4 :end 6}]]
+      (vec (set (flatten (report-overlapping events))))
+      )
+    )
+  )
 
 (comment
   (clojure.test/run-tests)
